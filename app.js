ddcd34c4bec55695e413eb9e74684ea5596c3224
Siva Manikanta <77544247+Manikanta937@users.noreply.github.com>
2023-04-04 11:23:49 +0530

    Create app.js

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var ucase=require('upper-case');
 http.createServer(function(req,res){
     res.writeHead(200,{'content-type':'text/html'});
     res.write(ucase.upperCase("This is manikanta and this is my first web application."));
-    return res.end("\n Thank You For Visiting My web-page :)!!!");
+    return res.end("\n Thank You For Visiting My page :)!!!");
 }).listen(3000);
